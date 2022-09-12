575e85f0b6426320966eb02c211c358cfbb34c15
Pablosatorre <80336428+Pablosatorre@users.noreply.github.com>
2022-09-12 23:05:38 +0200

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="mockup-phone border-primary">
       <div className="camera"></div>
       <div className="display">
-        <div className="artboard artboard-demo phone-1">Hi.</div>
+        <div className="artboard artboard-demo phone-1"></div>
       </div>
     </div>
   );
